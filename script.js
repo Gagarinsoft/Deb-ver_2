5c363f23c61d3e453251d7182e5cdacbe55a06b9
RomoBase <romosoft.base@gmail.com>
2023-11-01 12:36:56 +0200

    UI for header section

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ Array.prototype.lastElement = function() {
     return this[this.length - 1]
 }
 
+let guiElements = {
+    eventScore: {
+        left: document.getElementById('es-l'),
+        right: document.getElementById('es-r')
+    },
+    eventPoints: {
+        left: document.getElementById('ep-l'),
+        right: document.getElementById('ep-r')
+    },
+    gamePoints: {
+        left: document.getElementById('gp-l'),
+        right: document.getElementById('gp-r')
+    },
+    roundInfoPoints: document.getElementById('r_info_points'),
+    roundCount: document.getElementById('r_count')
+
+
+}
